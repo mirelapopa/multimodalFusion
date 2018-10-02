d3923e3a154b2074a5f1fb4cc9cac9bc9a485142
mirelapopa <mirela.popa@maastrichtuniversity.nl>
2018-10-02 10:31:40 +0200

    updated file for EHR and HETRA

--- code/downloadFileFromCloud.py
+++ code/downloadFileFromCloud.py
@@ -1,9 +1,19 @@
 from lib import S3FileManager
 
+#def downloadFile_Cloud(pathFile,fileName):    
+#	fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/mf')
+#    fileManager.download_file(pathFile,fileName)
+#    print 'Finished downloading the file'
+	
+def downloadFile_CloudEHR(pathFile):
+    fileManager = S3FileManager.S3FileManger('mf/out','mf/in/ehr')
+    #fileManager.download_file(pathFile,fileName)
+    fileManager.latest_document(pathFile) 
+    print 'Finished downloading the EHR file'
 
-def downloadFile_Cloud(pathFile,fileName):
-    #fileManager = S3FileManager.S3FileManger('mf/out/ehr','mf/in/hetra')
-    fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/mf')
-    fileManager.download_file(pathFile,fileName)
-    print 'Finished downloading the file'
+def downloadFile_CloudHETRA(pathFile):    
+    fileManager = S3FileManager.S3FileManger('mf/out','mf/in/hetra')
+    #fileManager.download_file(pathFile,fileName)
+    fileManager.latest_document(pathFile)  
+    print 'Finished downloading the HETRA file'
 
