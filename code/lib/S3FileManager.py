a8f19da2b6c5da7e5ba23c37027429bf7286c72d
mirelapopa <mirela.popa@maastrichtuniversity.nl>
2018-10-08 10:25:05 +0200

    added error handling

--- code/lib/S3FileManager.py
+++ code/lib/S3FileManager.py
@@ -14,9 +14,10 @@ class S3FileManger:
             response = s3.list_objects_v2(Bucket=self.BUCKET_NAME, Prefix=self.params.get("in_dir"))
             last_modified = sorted(response.get("Contents"), key=lambda content: content.get("LastModified"),
                                    reverse=True)
-            s3.download_file(self.BUCKET_NAME, last_modified[0].get("Key"), destination)
+            s3.download_file(self.BUCKET_NAME, destination, last_modified[0].get("Key"))
             return 1
         except Exception as e:
+            print e
             return 0
 
     def upload_file(self, origin, filename):
@@ -25,6 +26,7 @@ class S3FileManger:
             s3.upload_file(origin, self.BUCKET_NAME, self.params.get("out_dir") + '/' + filename)
             return 1
         except Exception as e:
+            print e
             return 0
 
     def download_file(self, destination, filename):
@@ -33,4 +35,5 @@ class S3FileManger:
             s3.download_file(self.BUCKET_NAME, self.params.get("in_dir") + '/' + filename, destination)
             return 1
         except Exception as e:
+            print e
             return 0
