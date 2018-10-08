a8dc03951b339fb5d48e2a72e603035afd1151a2
XanderZehcnas <30340513+XanderZehcnas@users.noreply.github.com>
2018-10-08 19:07:33 +0200

    Adjust library code

--- code/lib/S3FileManager.py
+++ code/lib/S3FileManager.py
@@ -11,15 +11,15 @@ class S3FileManger:
     def latest_document(self, destination):
         try:
             s3 = boto3.client("s3", region_name="eu-central-1")
-			print 'Bucket: ' + self.BUCKET_NAME
-			print 'Dir: ' + self.params.get("in_dir")
+print 'Bucket: ' + self.BUCKET_NAME
+print 'Dir: ' + self.params.get("in_dir")
             response = s3.list_objects_v2(Bucket=self.BUCKET_NAME, Prefix=self.params.get("in_dir"))
             last_modified = sorted(response.get("Contents"), key=lambda content: content.get("LastModified"),
                                    reverse=True)
             s3.download_file(self.BUCKET_NAME, destination, last_modified[0].get("Key"))
             return 1
         except Exception as e:
-			print e
+print e
             return 0
 
     def upload_file(self, origin, filename):
@@ -28,17 +28,17 @@ class S3FileManger:
             s3.upload_file(origin, self.BUCKET_NAME, self.params.get("out_dir") + '/' + filename)
             return 1
         except Exception as e:
-			print e
+print e
             return 0
 
     def download_file(self, destination, filename):
         try:
             s3 = boto3.client("s3", region_name="eu-central-1")
-			print 'Go into download file with params: ' + self.params.get("in_dir")
-			print 'Filename coming is:' + filename
-			print 'Target destination:' + destination
+print 'Go into download file with params: ' + self.params.get("in_dir")
+print 'Filename coming is:' + filename
+print 'Target destination:' + destination
             s3.download_file(self.BUCKET_NAME, self.params.get("in_dir") + '/' + filename, destination)
             return 1
         except Exception as e:
-			print e
+print e
             return 0
