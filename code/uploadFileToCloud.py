7f7d234aee30f1bf9a448544d7212ecda7b33703
mirelapopa <mirela.popa@maastrichtuniversity.nl>
2018-10-08 10:31:06 +0200

    updated error handling

--- code/uploadFileToCloud.py
+++ code/uploadFileToCloud.py
@@ -1,9 +1,13 @@
 from lib import S3FileManager
 
 def uploadFile_Cloud(pathFile,fileName):
-    fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/mf')
-    fileManager.upload_file(pathFile,fileName)
-    print 'Finished uploading the file to the cloud'
+    try: 
+        fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/mf')
+        f=fileManager.upload_file(pathFile,fileName)
+        if f:
+            print 'Finished uploading the file to the cloud'
+    except ValueError:
+        print ValueError
     
     
    
