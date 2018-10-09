4dc83ec3a716009730948881d73843475d11502b
XanderZehcnas <30340513+XanderZehcnas@users.noreply.github.com>
2018-10-09 08:04:17 +0200

    Adjust download file

--- code/lib/S3FileManager.py
+++ code/lib/S3FileManager.py
@@ -16,7 +16,7 @@ class S3FileManger:
             response = s3.list_objects_v2(Bucket=self.BUCKET_NAME, Prefix=self.params.get("in_dir"))
             last_modified = sorted(response.get("Contents"), key=lambda content: content.get("LastModified"),
                                    reverse=True)
-            s3.download_file(self.BUCKET_NAME, destination, last_modified[0].get("Key"))
+            s3.download_file(self.BUCKET_NAME, last_modified[0].get("Key"), destination)
             return 1
         except Exception as e:
             print e
