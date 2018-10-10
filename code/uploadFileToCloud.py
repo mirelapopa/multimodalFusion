177279ca163760c895d9cadba7985fa14948d233
mirelapopa <mirela.popa@maastrichtuniversity.nl>
2018-10-10 18:12:30 +0200

    updated output path

--- code/uploadFileToCloud.py
+++ code/uploadFileToCloud.py
@@ -2,7 +2,7 @@ from lib import S3FileManager
 
 def uploadFile_Cloud(pathFile,fileName):
     try: 
-        fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/mf')
+        fileManager = S3FileManager.S3FileManger('mf/out','ehr/out/dst')
         f=fileManager.upload_file(pathFile,fileName)
         if f:
             print 'Finished uploading the file to the cloud'
